62d5bf222ea149ccba9e20171d9775337487048a
Saul0604 <sagd08@gmail.com>
2025-05-11 23:36:45 -0600

    creación y prueba Animals(model)

--- app/src/main/java/com/example/animalsapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/animalsapp/screens/HomeScreen.kt
@@ -1,21 +1,64 @@
 package com.example.animalsapp.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.animalsapp.R
+import com.example.animalsapp.models.mockAnimals
+import com.example.animalsapp.ui.theme.AnimalsAppTheme
 
 
 @Composable
 fun HomeScreen(innerPadding: PaddingValues) {
+    val animals = mockAnimals
     Column(
-        modifier = Modifier.padding(innerPadding)
+        modifier = Modifier.fillMaxSize().padding(innerPadding),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Inicio", color = Color.White
-        )
+
+        Box(
+            modifier = Modifier
+                .size(180.dp)
+                .clip(CircleShape)
+        ) {
+            AsyncImage(
+                model = animals.image,
+                contentDescription = null,
+                placeholder = painterResource(R.drawable.ic_launcher_foreground),
+                error = painterResource(R.drawable.ic_launcher_foreground),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+
+
+
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    AnimalsAppTheme {
+        HomeScreen(innerPadding = PaddingValues(10.dp))
     }
 }
